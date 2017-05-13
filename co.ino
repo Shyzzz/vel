5546e94cac976ff0ebcf219e07fd0201140493f4
shyzzz <shyma107@gmail.com>
2017-05-13 20:27:55 +0530

    Add files via upload

---
+++ co.ino
@@ -0,0 +1,16 @@
+void setup() {
+Serial.begin(9600);
+pinMode(10,INPUT); pinMode(13,OUTPUT);
+}
+
+void loop(){
+  if(digitalRead(11)){
+    (digitalWrite(13,HIGH));
+    Serial.println("CO Detected");
+    delay(2000);
+    }
+  else{
+ Serial.println("CO Not Detected");
+  delay(1000);}
+
+}
---
+++ pir_code.ino
@@ -0,0 +1,17 @@
+void setup() {
+  Serial.begin(9600);
+  pinMode(11,INPUT); pinMode(13,OUTPUT);
+
+}
+
+void loop(){
+  while(digitalRead(11)){
+    (digitalWrite(13,HIGH));
+    Serial.println("PIR Detected");
+    delay(2000);
+    }
+//    else
+//      Serial.println("PIR Not Detected");
+  delay(1000);
+  }
+
